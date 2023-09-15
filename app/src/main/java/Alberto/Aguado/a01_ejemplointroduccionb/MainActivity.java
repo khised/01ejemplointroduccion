f8758c7703684110660f81281e93983559cc32f0
Alberto  Aguado Segovia <albertoaguadosegovia@iMac-07.local>
2023-09-15 10:39:56 +0200

    Initial commit

---
+++ app/src/main/java/Alberto/Aguado/a01_ejemplointroduccionb/MainActivity.java
@@ -0,0 +1,14 @@
+package Alberto.Aguado.a01_ejemplointroduccionb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
